f861638e062081f7655233b7f0c1637c253d2990
Maxime MILOIKOVITCH <maxime.miloikovitch1@etu.univ-lorraine.fr>
2025-04-02 09:12:01 +0200

    Complément des classes Lampe et Telecommande

--- src/Lampe.java
+++ src/Lampe.java
@@ -1,2 +1,17 @@
 public class Lampe {
+    private String nom;
+    private boolean allume;
+
+    public Lampe(String nom) {
+        this.nom = nom;
+        this.allume = false;
+    }
+
+    public void allumer(){
+        this.allume = true;
+    }
+
+    public void eteindre(){
+        this.allume = false;
+    }
 }
--- src/Telecommande.java
+++ src/Telecommande.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class Telecommande {
+
+    private ArrayList<Lampe> lampes;
+
+    public Telecommande() {
+        this.lampes = new ArrayList<Lampe>();
+    }
+
+    public void ajouterLampe(Lampe lampe) {
+        this.lampes.add(lampe);
+    }
+
+    public void activerLampe(int index) {
+        this.lampes.get(index).allumer();
+    }
+
+    public void desactiverLampe(int index) {
+        this.lampes.get(index).eteindre();
+    }
+
+    public void activerTout() {
+    }
 }
