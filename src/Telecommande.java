8ba73cbd5b515d473098fa8084e758e866cdef64
Maxime MILOIKOVITCH <maxime.miloikovitch1@etu.univ-lorraine.fr>
2025-04-22 14:35:41 +0200

    Hifi

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -1,34 +1,61 @@
 import java.util.ArrayList;
 
 public class Telecommande {
-
-    private ArrayList<Lampe> lampes;
+    private ArrayList <Lampe> lampes;
+    private ArrayList <Hifi> hifis;
 
     public Telecommande() {
-        this.lampes = new ArrayList<Lampe>();
+        this.lampes = new ArrayList <Lampe>();
+        this.hifis = new ArrayList<Hifi>();
     }
 
     public void ajouterLampe(Lampe lampe) {
         this.lampes.add(lampe);
     }
 
-    public void activerLampe(int index) {
-        this.lampes.get(index).allumer();
+    public void activerLampe(int indiceLampe) {
+        this.lampes.get(indiceLampe).allumer();
     }
 
-    public void desactiverLampe(int index) {
-        this.lampes.get(index).eteindre();
+    public void desactiverLampe(int indiceLampe) {
+        this.lampes.get(indiceLampe).eteindre();
+    }
+
+    public ArrayList getLampes() {
+        return this.lampes;
+    }
+
+    public void ajouterHifi(Hifi hifi) {
+        this.hifis.add(hifi);
+    }
+
+    public void activerHifi(int indiceHifi) {
+        this.hifis.get(indiceHifi).allumer();
+    }
+
+    public void desactiverHifi(int indiceHifi) {
+        this.hifis.get(indiceHifi).eteindre();
+    }
+
+    public ArrayList getHifis() {
+        return this.hifis;
     }
 
     public void activerTout() {
-        throw new Error ( " code non ecrit " );
+        for (Lampe lampe : this.lampes) {
+            lampe.allumer();
+        }
+        for (Hifi hifi : this.hifis) {
+            hifi.allumer();
+        }
     }
 
-    public String toString(){
-        throw new Error ( " code non ecrit " );
-    }
-
-    public ArrayList<Lampe> getLampes() {
-        return this.lampes;
+    public String toString() {
+        String res = "Telecommande:\n";
+        res += "Lampes:\n";
+        for (int i = 0; i < this.lampes.size(); i++) {
+            res += "Lampe (" + i + ") " + this.lampes.get(i).toString() + "\n";
+        }
+        return res;
     }
 }
